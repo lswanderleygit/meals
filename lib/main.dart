9b7d49c80ba1f6797e4cb09dbc53f6579b954cd3
Lucas <lucaswanderley4@gmail.com>
2020-08-19 21:21:56 -0300

    implements favorite meal

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,10 @@ import 'screens/meal_detail_screen.dart';
 import 'screens/settinigs_screen.dart';
 
 import 'utils/app_routes.dart';
+
 import 'models/meal.dart';
 import 'data/dummy_data.dart';
+import 'models/settings.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,7 +20,41 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  Settings settings = Settings();
   List<Meal> _availableMeals = DUMMY_MEALS;
+  List<Meal> _favoriteMeals = [];
+
+  void _filterMeals(Settings settings) {
+    setState(
+      () {
+        this.settings = settings;
+
+        _availableMeals = DUMMY_MEALS.where((meal) {
+          final filterGluten = settings.isGlutenFree && !meal.isGlutenFree;
+          final filterLactose = settings.isLactoseFree && !meal.isLactoseFree;
+          final filterVegan = settings.isVegan && !meal.isVegan;
+          final filterVegetarian = settings.isVegetarian && !meal.isVegetarian;
+
+          return !filterGluten &&
+              !filterLactose &&
+              !filterVegan &&
+              !filterVegetarian;
+        }).toList();
+      },
+    );
+  }
+
+  void _toggleFavorite(Meal meal) {
+    setState(() {
+      _favoriteMeals.contains(meal)
+          ? _favoriteMeals.remove(meal)
+          : _favoriteMeals.add(meal);
+    });
+  }
+
+  bool _isFavorite(Meal meal) {
+    return _favoriteMeals.contains(meal);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,11 +75,12 @@ class _MyAppState extends State<MyApp> {
       ),
       // home: CategoriesScreen(),
       routes: {
-        AppRoutes.HOME: (ctx) => TabsScreen(),
+        AppRoutes.HOME: (ctx) => TabsScreen(_favoriteMeals),
         AppRoutes.CATEGORIES_MEALS: (ctx) =>
             CategoryMealsScreen(_availableMeals),
-        AppRoutes.MEAL_DETAILS: (ctx) => MealDetailScreen(),
-        AppRoutes.SETTINGS: (ctx) => SettingsScreen(),
+        AppRoutes.MEAL_DETAILS: (ctx) =>
+            MealDetailScreen(_toggleFavorite, _isFavorite),
+        AppRoutes.SETTINGS: (ctx) => SettingsScreen(_filterMeals, settings),
       },
       onGenerateRoute: (settings) {
         if (settings.name == '/alguma-coisa') {
--- lib/screens/favorite_screen.dart
+++ lib/screens/favorite_screen.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
 
+import '../components/meal_item.dart';
+import '../models/meal.dart';
+
 class FavoriteScreen extends StatelessWidget {
+  final List<Meal> favoriteMeals;
+
+  const FavoriteScreen(this.favoriteMeals);
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Minha refeições Favoritas!'),
-    );
+    if (favoriteMeals.isEmpty) {
+      return Center(
+        child: Text('Minha refeições Favoritas!'),
+      );
+    } else {
+      return ListView.builder(
+        itemCount: favoriteMeals.length,
+        itemBuilder: (ctx, index) {
+          return MealItem(favoriteMeals[index]);
+        },
+      );
+    }
   }
 }
